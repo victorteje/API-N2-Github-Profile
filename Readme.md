de8109c97082caa0f2b85d3f9e2c6e67d0de46f5
victorteje <vteje2@gmail.com>
2023-06-28 11:01:34 +0200

    updated README

--- Readme.md
+++ Readme.md
@@ -1,1 +1,3 @@
-Good Luck!
+A GitHub user search engine.
+
+Requires Axios. To install, run "npm install axios" on the console.
--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-const APIURL = 'https://api.github.com/users/'
+// const APIURL = 'https://api.github.com/users/'
+
+axios.get('https://api.github.com/users/')
+.then((response)) => {
+    
+}
+
+.catch((error)) => {
+    console.log("No user with that name");
+}
