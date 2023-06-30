a03fa213d667819bb2d836b7b7bd306b548e7c65
victorteje <vteje2@gmail.com>
2023-06-30 22:41:12 +0200

    working on card

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-// const APIURL = 'https://api.github.com/users/'
 
-axios.get('https://api.github.com/users/')
-.then((response)) => {
-    
+
+let getData = () => {
+axios.get("https://api.github.com/users")
+.then(response => {
+  const gitUsers = response.data;
+  console.log(gitUsers);
+  
+  gitUsers.forEach(user => {
+    const profile = `
+      <section class="card">
+        <img class="avatar" src=${user.avatar_url}>
+        <div class= "user-info">
+          <h2>${user.login}</h2>
+          <ul>
+            <li>${user.followers_url}</li>
+            <li>${user.following_url}</li>
+            <li>${user.repos_url}</li>
+          </ul>
+        </div>
+      </section>
+    `;
+    let printProfile = document.getElementById("main")
+    printProfile.innerHTML = profile;
+  });
+})
+.catch(err => {
+  console.log(err);
+})
 }
 
-.catch((error)) => {
-    console.log("No user with that name");
-}
+getData();
