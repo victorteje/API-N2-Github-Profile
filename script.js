c32020146cfaace691f6ac8965fa449990ece482
victorteje <vteje2@gmail.com>
2023-07-01 20:19:12 +0200

    search function works

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
+const form = document.getElementById("form");
 
-
-let getData = () => {
-axios.get("https://api.github.com/users")
-.then(response => {
-  const gitUsers = response.data;
-  console.log(gitUsers);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const userName = document.getElementById("search").value;
   
-  gitUsers.forEach(user => {
-    const profile = `
-      <section class="card">
-        <img class="avatar" src=${user.avatar_url}>
-        <div class= "user-info">
-          <h2>${user.login}</h2>
-          <ul>
-            <li>${user.followers_url}</li>
-            <li>${user.following_url}</li>
-            <li>${user.repos_url}</li>
-          </ul>
-        </div>
-      </section>
-    `;
-    let printProfile = document.getElementById("main")
-    printProfile.innerHTML = profile;
+  const APIURL = `https://api.github.com/users/${userName}`;
+  
+  axios.get(APIURL).then(response => {
+    const userData = response.data;
+    console.log(userData);
+    
+      const profile = `
+        <section class="card">
+          <img class="avatar" src=${userData.avatar_url}>
+          <div class= "user-info">
+            <h2>${userData.login}</h2>
+            <ul>
+              <li>${userData.followers} followers</li>
+              <li> - ${userData.following} following</li>
+              <li> - ${userData.public_repos} repositories</li>
+            </ul>
+          </div>
+        </section>
+      `;
+      const printProfile = document.getElementById("main")
+      printProfile.innerHTML = profile;
   });
-})
-.catch(err => {
-  console.log(err);
-})
-}
-
-getData();
+    
+});
