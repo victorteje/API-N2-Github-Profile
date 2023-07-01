8fd6881619aad0a27e03a73f04f747cfe1f163bf
victorteje <vteje2@gmail.com>
2023-07-01 22:09:21 +0200

    added bio to card

--- script.js
+++ script.js
@@ -10,21 +10,21 @@ form.addEventListener("submit", (e) => {
     const userData = response.data;
     console.log(userData);
     
-      const profile = `
-        <section class="card">
-          <img class="avatar" src=${userData.avatar_url}>
-          <div class= "user-info">
-            <h2>${userData.login}</h2>
-            <ul>
-              <li>${userData.followers} followers</li>
-              <li> - ${userData.following} following</li>
-              <li> - ${userData.public_repos} repositories</li>
-            </ul>
-          </div>
-        </section>
-      `;
-      const printProfile = document.getElementById("main")
-      printProfile.innerHTML = profile;
+    const profile = document.getElementById("main");
+    profile.innerHTML = `
+      <section class="card">
+        <img class="avatar" src=${userData.avatar_url}>
+        <div class= "user-info">
+          <h2>${userData.name}</h2>
+          <p>${userData.bio || "This user has not submitted a bio."}</p>
+          <ul>
+            <li>${userData.followers} followers</li>
+            <li>${userData.following} following</li>
+            <li>${userData.public_repos} repositories</li>
+          </ul>
+          <div class= "repo">${userData.repos}</div>
+        </div>
+      </section>
+    `;
   });
-    
 });
